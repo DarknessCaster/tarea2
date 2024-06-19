1594bb6c7369d8f063f6d054f16b6d50a8632f3e
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-19 15:31:04 -0400

    ola

--- nodo.cpp
+++ nodo.cpp
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
-int main(){
+int opcion = 0;
 
+int main(){
+    do {
+        printf("\n================== MENU ==================\n");
+        printf("1. Enviar mensaje de prueba\n");
+        printf("2. Enviar mensaje de texto\n");
+        printf("3. Buscar archivo txt\n");
+        printf("4. Ver cantidad de mensajes enviados\n");
+        printf("5. Listar archivos de texto del receptor\n");
+        printf("6. Crear archivo y registrar un mensaje\n"); //  *** PUNTAJES EXTRAS: 2.
+        printf("7. Cerrar programa receptor\n");
+        printf("==============================================\n");
+        printf("Ingrese la opcion deseada: ");
+        scanf("%d", &opcion);
+        getchar(); 
+        switch (opcion) {
+            case 1:
+                printf("Hola mundo");
+                break;
+            default:
+                break;
+        }
+    } while (opcion != 7);
     return 0;
-}
+    }
