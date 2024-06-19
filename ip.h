93edb527194419b4372a9b861d4e0500a2f72239
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-19 15:54:28 -0400

    ola

--- ip.h
+++ ip.h
@@ -5,15 +5,15 @@
 #define BYTE unsigned char
 
 // Definimos la estructura que representa el protocolo IPv4 modificado
-struct ipv4_modificado {
+struct IP {
     BYTE flag_fragmento;       // 8 bits para el flag de fragmento
     BYTE offset_fragmento;     // 8 bits para el offset de fragmento
     BYTE ttl;                  // 8 bits para TTL
-    BYTE identificacion;      // 16 bits para identificación
-    BYTE longitud_datos;      // 16 bits para longitud de los datos
-    BYTE suma_verificacion;   // 16 bits para la suma de verificación
-    BYTE ip_origen;           // 32 bits para la IP de origen
-    BYTE ip_destino;          // 32 bits para la IP de destino
-    BYTE datos[];              // Arreglo de datos (tamaño variable)
+    BYTE identificacion[2];    // 16 bits para identificación
+    BYTE longitud_datos[2];    // 16 bits para longitud de los datos
+    BYTE suma_verificacion[2]; // 16 bits para la suma de verificación
+    BYTE ip_origen[4];         // 32 bits para la IP de origen
+    BYTE ip_destino[4];        // 32 bits para la IP de destino
+    BYTE datos[1500];          // Arreglo de datos (tamaño variable, máximo 1500 bytes)
 };
 #endif
