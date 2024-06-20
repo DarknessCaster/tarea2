7ddd2c74c986836d89d95b8a09aad50e0c6b0b76
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-20 05:22:11 -0400

    ola

--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -20,13 +20,26 @@ int main(int nargs, char* arg_arr[]){
         char* ip_nodo = arg_arr[1];
         char* puerto_tx = arg_arr[2];
         char* puerto_rx = arg_arr[3];
+
         printf("IP del nodo: %s", ip_nodo);
+        
         FILE *rx_stream = fopen(puerto_rx, "r");
         FILE *tx_stream = fopen(puerto_tx, "w");
 
         if(strcmp(ip_nodo, ip_A) == 0){
             printf("Nodo A iniciado correctamente\n");
-            printf("========= MENU =========");
+        }
+        else if(strcmp(ip_nodo, ip_B) == 0){
+            printf("Nodo B iniciado correctamente\n");
+        }
+        else if(strcmp(ip_nodo, ip_C) == 0){
+            printf("Nodo C iniciado correctamente\n");
+        }
+        else if(strcmp(ip_nodo, ip_D) == 0){
+            printf("Nodo D iniciado correctamente\n");
+        }
+        else if(strcmp(ip_nodo, ip_E) == 0){
+            printf("Nodo E iniciado correctamente\n");
         }
     } else{
         printf("Utilizar: ");
