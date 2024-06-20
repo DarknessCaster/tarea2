4df1a6c6a1577282b24896811c2579100960e670
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-20 05:08:17 -0400

    ola

--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -28,27 +28,6 @@ int main(int nargs, char* arg_arr[]){
             printf("Nodo A iniciado correctamente\n");
             printf("========= MENU =========");
         }
-        do {
-            printf("\n================== MENU ==================\n");
-            printf("1. Enviar mensaje de prueba\n");
-            printf("2. Enviar mensaje de texto\n");
-            printf("3. Buscar archivo txt\n");
-            printf("4. Ver cantidad de mensajes enviados\n");
-            printf("5. Listar archivos de texto del receptor\n");
-            printf("6. Crear archivo y registrar un mensaje\n"); //  *** PUNTAJES EXTRAS: 2.
-            printf("7. Cerrar programa receptor\n");
-            printf("==============================================\n");
-            printf("Ingrese la opcion deseada: ");
-            scanf("%d", &opcion);
-            getchar(); 
-            switch (opcion) {
-                case 1:
-                    printf("Hola mundo");
-                    break;
-                default:
-                    break;
-            }
-        } while (opcion != 7);
     } else{
         printf("Utilizar: ");
     }
