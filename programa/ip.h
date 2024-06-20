0f9459dd85b6712decf511a906fe31196f7057e3
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-20 04:43:17 -0400

    ola

--- programa/ip.h
+++ programa/ip.h
@@ -8,8 +8,8 @@
 struct IP {
     BYTE flag_fragmento;       // 8 bits para el flag de fragmento
     BYTE offset_fragmento;     // 8 bits para el offset de fragmento
-    BYTE ttl;                  // 8 bits para TTL
-    BYTE identificacion[2];    // 16 bits para identificación
+    BYTE TTL;                  // 8 bits para TTL
+    BYTE identificacion;    // 16 bits para identificación
     BYTE longitud_datos[2];    // 16 bits para longitud de los datos
     BYTE suma_verificacion[2]; // 16 bits para la suma de verificación
     BYTE ip_origen[4];         // 32 bits para la IP de origen
