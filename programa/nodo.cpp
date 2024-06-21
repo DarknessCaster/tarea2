97cddb1273cf11a92365c1d9fb05b45599426d99
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-21 04:42:15 -0400

    ola

--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -57,7 +57,7 @@ int main(int nargs, char* arg_arr[]){
                     
                     // se guarda longitud de datos
                     len = strlen((const char*)paquete.datos);
-                    paquete.datos[len-1] = "\0"; // Arreglo para elimminar \n al final de datos
+                    paquete.datos[len-1] = (BYTE)"\0"; // Arreglo para elimminar \n al final de datos
                     paquete.lng_datos[0] = (BYTE)(len >> 8); // Byte alto
                     paquete.lng_datos[1] = (BYTE)(len & 0xFF); // Byte bajo
                     printf("Longitud de datos: %d\n", (paquete.lng_datos[0] << 8) | paquete.lng_datos[1]);
@@ -65,7 +65,6 @@ int main(int nargs, char* arg_arr[]){
                     paquete.id = contador_id; // se guarda identificacion de paquete
                     printf("ID: %d\n", paquete.id);
                     contador_id++;
-                    printf("");
                     break;
                 case 2: // NODO C
                     convertir_ip(ip_C, paquete.ip_destino);
