b8a8de79d3aa4423de083f94094b4c0e89e79f69
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-21 01:43:57 -0400

    ola

--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -37,7 +37,7 @@ int main(int nargs, char* arg_arr[]){
             scanf("%d", &opcion);
             switch (opcion) {
                 case 1:
-                    convertir_ip(ip_B, paquete.ip_destino);
+                    strcpy(paquete.ip_origen, ipprueba);
                     imprimir_ip(paquete.ip_destino);
 
                     break;
