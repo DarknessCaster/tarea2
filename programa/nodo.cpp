eb3731ea26e4d6bf5c73c2158e9c62876a8ebbc4
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-21 04:49:51 -0400

    ola

--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -57,7 +57,7 @@ int main(int nargs, char* arg_arr[]){
                     
                     // se guarda longitud de datos
                     len = strlen((const char*)paquete.datos);
-                    paquete.datos[len-1] = (unsigned char*)"\0"; // Arreglo para elimminar \n al final de datos
+                    paquete.datos[len-1] = (unsigned char)"\0"; // Arreglo para elimminar \n al final de datos
                     paquete.lng_datos[0] = (BYTE)(len >> 8); // Byte alto
                     paquete.lng_datos[1] = (BYTE)(len & 0xFF); // Byte bajo
                     printf("Longitud de datos: %d\n", (paquete.lng_datos[0] << 8) | paquete.lng_datos[1]);
