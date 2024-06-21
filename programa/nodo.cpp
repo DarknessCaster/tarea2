b94685054e3cae7b13470e3a96fedf63168deff2
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-20 21:30:13 -0400

    ola

--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -29,75 +29,75 @@ int main(int nargs, char* arg_arr[]){
         scanf("%d", &opcion);
         printf("%d", opcion);
         getchar();
-        if(strcmp(ip_nodo, ip_A) == 0){
-            convertir_ip(ip_A, paquete.ip_origen);
-            imprimir_ip(paquete.ip_origen);
-            printf("\nNodo A iniciado correctamente\n");
-            printf("A quien desea enviar el mensaje?\n");
-            printf("1. B\n2. C\n3. D\n4. E\n5. A todos (broadcast)\n");
-            printf("Ingrese una opcion: ");
+        // if(strcmp(ip_nodo, ip_A) == 0){
+        //     convertir_ip(ip_A, paquete.ip_origen);
+        //     imprimir_ip(paquete.ip_origen);
+        //     printf("\nNodo A iniciado correctamente\n");
+        //     printf("A quien desea enviar el mensaje?\n");
+        //     printf("1. B\n2. C\n3. D\n4. E\n5. A todos (broadcast)\n");
+        //     printf("Ingrese una opcion: ");
 
-            scanf("%d", &opcion);
-            printf("%d", opcion);
-            switch (opcion) {
-                case 1:
-                    convertir_ip(ip_B, paquete.ip_destino);
-                    imprimir_ip(paquete.ip_destino);
-                    break;
-                case 2:
-                    //paquete.ip_destino = ip_C;
-                    break;
-                case 3:
-                    //paquete.ip_destino = ip_D;
-                    break;
-                case 4:
-                    //paquete.ip_destino = ip_E;
-                    break;
-                case 5:
-                    //paquete.ip_destino = ip_broadcast;
-                    break;
-                default:
-                    return 1;
-            }
-        }
-        else if(strcmp(ip_nodo, ip_B) == 0){
-            convertir_ip(ip_B, paquete.ip_origen);
-            imprimir_ip(paquete.ip_origen);
-            printf("\nNodo B iniciado correctamente\n");
-            printf("A quien desea enviar el mensaje?\n");
-            printf("1. A\n2. C\n3. D\n4. E\n5. A todos (broadcast)\n");
-            printf("Ingrese una opcion: ");
-            scanf("%d", &opcion);
-            if(opcion == 1){
-                convertir_ip(ip_A, paquete.ip_destino);
-                imprimir_ip(paquete.ip_destino);
-            }
-            else if(opcion == '2'){
+        //     scanf("%d", &opcion);
+        //     printf("%d", opcion);
+        //     switch (opcion) {
+        //         case 1:
+        //             convertir_ip(ip_B, paquete.ip_destino);
+        //             imprimir_ip(paquete.ip_destino);
+        //             break;
+        //         case 2:
+        //             //paquete.ip_destino = ip_C;
+        //             break;
+        //         case 3:
+        //             //paquete.ip_destino = ip_D;
+        //             break;
+        //         case 4:
+        //             //paquete.ip_destino = ip_E;
+        //             break;
+        //         case 5:
+        //             //paquete.ip_destino = ip_broadcast;
+        //             break;
+        //         default:
+        //             return 1;
+        //     }
+        // }
+        // else if(strcmp(ip_nodo, ip_B) == 0){
+        //     convertir_ip(ip_B, paquete.ip_origen);
+        //     imprimir_ip(paquete.ip_origen);
+        //     printf("\nNodo B iniciado correctamente\n");
+        //     printf("A quien desea enviar el mensaje?\n");
+        //     printf("1. A\n2. C\n3. D\n4. E\n5. A todos (broadcast)\n");
+        //     printf("Ingrese una opcion: ");
+        //     scanf("%d", &opcion);
+        //     if(opcion == 1){
+        //         convertir_ip(ip_A, paquete.ip_destino);
+        //         imprimir_ip(paquete.ip_destino);
+        //     }
+        //     else if(opcion == '2'){
 
-            }
-        }
-        else if(strcmp(ip_nodo, ip_C) == 0){
-            printf("\nNodo C iniciado correctamente\n");
-            printf("A quien desea enviar el mensaje?\n");
-            printf("1. A\n2. B\n3. D\n4. E\n5. A todos (broadcast)\n");
-            printf("Ingrese una opcion: ");
-            scanf("%d", &opcion);
-        }
-        else if(strcmp(ip_nodo, ip_D) == 0){
-            printf("\nNodo D iniciado correctamente\n");
-            printf("A quien desea enviar el mensaje?\n");
-            printf("1. A\n2. B\n3. C\n4. E\n5. A todos (broadcast)\n");
-            printf("Ingrese una opcion: ");
-            scanf("%d", &opcion);
-        }
-        else if(strcmp(ip_nodo, ip_E) == 0){
-            printf("\nNodo E iniciado correctamente\n");
-            printf("A quien desea enviar el mensaje?\n");
-            printf("1. A\n2. B\n3. C\n4. D\n5. A todos (broadcast)\n");
-            printf("Ingrese una opcion: ");
-            scanf("%d", &opcion);
+        //     }
+        // }
+        // else if(strcmp(ip_nodo, ip_C) == 0){
+        //     printf("\nNodo C iniciado correctamente\n");
+        //     printf("A quien desea enviar el mensaje?\n");
+        //     printf("1. A\n2. B\n3. D\n4. E\n5. A todos (broadcast)\n");
+        //     printf("Ingrese una opcion: ");
+        //     scanf("%d", &opcion);
+        // }
+        // else if(strcmp(ip_nodo, ip_D) == 0){
+        //     printf("\nNodo D iniciado correctamente\n");
+        //     printf("A quien desea enviar el mensaje?\n");
+        //     printf("1. A\n2. B\n3. C\n4. E\n5. A todos (broadcast)\n");
+        //     printf("Ingrese una opcion: ");
+        //     scanf("%d", &opcion);
+        // }
+        // else if(strcmp(ip_nodo, ip_E) == 0){
+        //     printf("\nNodo E iniciado correctamente\n");
+        //     printf("A quien desea enviar el mensaje?\n");
+        //     printf("1. A\n2. B\n3. C\n4. D\n5. A todos (broadcast)\n");
+        //     printf("Ingrese una opcion: ");
+        //     scanf("%d", &opcion);
 
-        }
+        // }
         // fclose(vport_tx);
         // fclose(vport_rx);
     } else{
