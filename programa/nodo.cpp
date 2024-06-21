bed9dd5fb799c95c22bf3e03bff70aef904362e9
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-20 22:08:21 -0400

    ola

--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -26,7 +26,7 @@ int main(int nargs, char* arg_arr[]){
         FILE *vport_rx = fopen(puerto_rx, "r");
         FILE *vport_tx = fopen(puerto_tx, "w");
         BYTE ipprueba[4] = {0x65, 0x65, 0x65, 0x65};
-        printf("%c.%c.%c.%c", ipprueba);
+        imprimir_ip(ipprueba);
         if(strcmp(ip_nodo, ip_A) == 0){
             convertir_ip(ip_A, paquete.ip_origen);
             imprimir_ip(paquete.ip_origen);
