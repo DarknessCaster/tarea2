650c5125f8b30d707357ff79aed723efdf49470d
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-23 02:59:48 -0400

    ola

--- programa/funcionesIP.h
+++ programa/funcionesIP.h
@@ -1,7 +1,7 @@
 #ifndef FUNCIONESIP_H
 #define FUNCIONESIP_H
 
-int encapsularIP(IP &paquete, BYTE TTL, int id, const char* ip_origen, const char* ip_destino);
+int encapsularIP(IP &paquete, BYTE TTL, int id, BYTE ip_origen[4], BYTE ip_destino[4]);
 void convertir_ip(const char* ip_str, BYTE ip[4]);
 void imprimir_ip(BYTE ip[4]);
 bool desempaquetarIP(IP &paquete);
--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -166,7 +166,7 @@ int main(int nargs, char* arg_arr[]){
                                 printf("Mensaje enviado por el nodo %X\n", paquete_rx.ip_origen[0]);
                                 printf("%s", paquete_rx.datos);
                                 paquete_rx.TTL--;
-                                encapsularIP(paquete_rx, paquete_rx.TTL, int(paquete_rx.id),);
+                                encapsularIP(paquete_rx, paquete_rx.TTL, int(paquete_rx.id), paquete_rx.ip_origen, paquete_rx.ip_destino);
                                 writeSlip(paquete_rx.FRAMES, len_rx, vport_tx); // ENVIAR POR SLIP
                             } else {
                                 printf("El mensaje broadcast es propio, se descarta.\n");
