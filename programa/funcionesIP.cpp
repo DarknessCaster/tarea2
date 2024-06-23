939b4b3f6ad15a5a9cd3c050b43f523510926839
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-23 05:48:29 -0400

    ola

--- programa/funcionesIP.cpp
+++ programa/funcionesIP.cpp
@@ -173,6 +173,11 @@ void menu_recibir(FILE *vport_tx, FILE *vport_rx, BYTE ip_Nodo[4], BYTE ips[6][4
                     printf("El mensaje broadcast es propio, se descarta.\n");
                 }
             }
+            else{
+                paquete_rx.TTL--;
+                encapsularIP(paquete_rx, paquete_rx.TTL, paquete_rx.id, paquete_rx.ip_origen, paquete_rx.ip_destino);
+                writeSlip(paquete_rx.FRAMES, len_rx, vport_tx); // ENVIAR POR SLIP
+            }
         }
     }
 }
