2b8f5ae2674adeff7de03a75e95cbf74ebdff093
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-23 21:06:39 -0400

    ola

--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -76,7 +76,21 @@ int main(int nargs, char* arg_arr[]){
         fclose(vport_tx);
         fclose(vport_rx);
     } else{
-        printf("Utilizar: \n");
+        printf("MANUAL DE USUARIO:\n");
+        printf("-------------------\n");
+        printf("Para ejecutar el programa debe inicializar un nodo con su respectiva letra usando el comando:\n");
+        printf("\t make run_nodo_(Letra del nodo)\n\n");
+        printf("Ejemplos:\n");
+        printf("\t Nodo A:\t make run_nodo_A\n");
+        printf("\t Nodo B:\t make run_nodo_B\n");
+        printf("\t Nodo C:\t make run_nodo_C\n");
+        printf("\t Nodo D:\t make run_nodo_D\n");
+        printf("\t Nodo E:\t make run_nodo_E\n\n");
+        printf("Descripción del Programa:\n");
+        printf("-------------------------\n");
+        printf("Este programa simula una red de nodos interconectados en un anillo. Cada nodo puede enviar o recibir\n");
+        printf("mensajes a otros nodos a través de un protocolo IP modificado. Los mensajes pueden ser unicast (a un solo nodo)\n");
+        printf("o broadcast (a todos los nodos).\n\n");
     }
     return 0;
 }
