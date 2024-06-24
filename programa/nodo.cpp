aa535133f605e063208bfc35520fdf93d199601c
DarknessCaster <miguelfuentes2014fs@gmail.com>
2024-06-23 21:44:00 -0400

    ola

--- programa/nodo.cpp
+++ programa/nodo.cpp
@@ -69,9 +69,14 @@ int main(int nargs, char* arg_arr[]){
             else if(memcmp(ips[4], ip_Nodo, 4) == 0){
                 menu_recibir(vport_tx, vport_rx, ip_Nodo, ips);
             }
+        else if (opcion == 3) {
+            // Salir del programa
+            printf("Programa finalizado correctamente.\n");
+            break;
         }
-        else{
-            exit(1);
+        else {
+            printf("Opción no válida. Por favor ingrese 1, 2 o 3.\n");
+            
         }
         fclose(vport_tx);
         fclose(vport_rx);
